6a8bec2d55a385c0659dc8554fcb59dc2385ab1e
Amy DiPasquale <adipasquale@fearless.tech>
2022-05-11 09:24:06 -0400

    pluralize Users

--- blueprintapi/settings.py
+++ blueprintapi/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "users.apps.UserConfig"
+    "users.apps.UsersConfig"
 ]
 
 MIDDLEWARE = [
