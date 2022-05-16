2fe90018e770fbb28dd55305f4da8a34cbd1566b
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-05-16 13:26:55 -0400

    removed leading slash in url path

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,5 @@ from .views import ProjectsDetailView, ProjectsListViews
 
 urlpatterns = [
     path("", ProjectsListViews.as_view()),
-    path("/<int:project_id>/", ProjectsDetailView.as_view()),
+    path("<int:project_id>/", ProjectsDetailView.as_view()),
 ]
