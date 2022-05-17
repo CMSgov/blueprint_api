28ed2c788d77951d2b81aa06eaa14f2a38e4f17a
Amy DiPasquale <adipasquale@fearless.tech>
2022-05-17 10:56:44 -0400

    switch to use drf's built in api views

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import UsersDetailView, UsersListViews
+from .views import UserDetailView, UserListView
 
 urlpatterns = [
-    path("", UsersListViews.as_view()),
-    path("<int:user_id>/", UsersDetailView.as_view()),
+    path("", UserListView.as_view(), name="user-list"),
+    path("<int:pk>/", UserDetailView.as_view(), name="user-detail"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,56 +1,18 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from .models import User
 from .serializers import UserSerializer
 
 
-class UsersListViews(APIView):
-
-    # @api_view('POST')
-    def post(self, request, *args, **kwargs):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # @api_view('GET')
-    def get(request, *args, **kwargs):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# Use for read-write endpoints to represent a collection of model instances.
+# Provides get and post method handlers.
+class UserListView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-class UsersDetailView(APIView):
-    def get_object(self, user_id):
-        try:
-            return User.objects.get(id=user_id)
-        except User.DoesNotExist:
-            return None
-
-    def get(self, request, user_id, *args, **kwargs):
-        user_instance = self.get_object(user_id)
-        if not user_instance:
-            return Response(
-                {"response": "The user you are looking for does not exist"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        serializer = UserSerializer(user_instance)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def patch(self, request, user_id, *args, **kwargs):
-        user_instance = self.get_object(user_id)
-        if not user_instance:
-            return Response(
-                {"response": "The user you are looking for does not exist"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        serializer = UserSerializer(
-            instance=user_instance, data=request.data, partial=True
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Use for read or update endpoints to represent a single model instance.
+# Provides get, put, and patch method handlers.
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
