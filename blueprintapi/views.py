17c90b219a2a5219acc4993066275ee3491096e7
Tom Camp <tom.camp@civicactions.com>
2022-06-08 11:50:44 -0400

    Adding rest_framework.

--- blueprintapi/views.py
+++ blueprintapi/views.py
@@ -1,12 +1,15 @@
 from django.db import OperationalError, connection
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(["GET"])
 def index(request):
     response = {"content": "Home"}
-    return JsonResponse(response)
+    return Response(response)
 
 
+@api_view(["GET"])
 def healthcheck(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT 1")
@@ -14,4 +17,4 @@ def healthcheck(request):
         if one != 1:
             raise OperationalError("Could not connect to database")
     response = {"content": "Healthy"}
-    return JsonResponse(response)
+    return Response(response)
