95bdbbddb851a9394a567b37046c5634774c5dbb
Tom Camp <tom.camp@civicactions.com>
2022-06-28 15:55:06 -0400

    Pre-commits and DEBUG value fixes.

--- blueprintapi/settings.py
+++ blueprintapi/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-from typing import List
+
 from .Environment import Environment
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from django.contrib.auth.models import Group
 from django.db import models
 from django.db.models.signals import post_save
