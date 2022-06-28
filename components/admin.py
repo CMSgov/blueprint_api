c43aeb0b1d3dc8c91386c86b35f7c046056e1b3e
Tom Camp <tom.camp@civicactions.com>
2022-06-28 11:31:49 -0400

    Adding id to admin display.

--- components/admin.py
+++ components/admin.py
@@ -7,4 +7,4 @@ from .models import Component
 @admin.register(Component)
 class ComponentAdmin(admin.ModelAdmin):
     form = ComponentAdminForm
-    list_display = ("title", "catalog", "created", "updated")
+    list_display = ("id", "title", "catalog", "created", "updated")
