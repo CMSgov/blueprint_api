47243f1c347fd1c1f29c4e4bfa1af7fb412d4c14
Tom Camp <tom.camp@civicactions.com>
2022-07-13 10:08:22 -0400

    Adding validator.

--- catalogs/models.py
+++ catalogs/models.py
@@ -1,13 +1,33 @@
+import json
 import os
 
+import jsonschema
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.dispatch import receiver
+
+
+def validate_catalog(file_name):
+    cat_upload = file_name
+    cat = json.load(cat_upload.file)
+    print("CATALOG")
+    print(cat)
+    with open("catalogs/schemas/oscal_catalog_schema.json", "r") as file:
+        oscal_schema = json.load(file)
+
+    try:
+        print("validating")
+        jsonschema.validate(instance=cat, schema=oscal_schema)
+    except ValidationError:
+        raise ValidationError("The Catalog is not a valid OSCAL catalog.")
 
 
 class Catalog(models.Model):
     name = models.CharField(max_length=100, help_text="Name of Catalog", unique=True)
     file_name = models.FileField(
-        max_length=100, help_text="Location of static catalog data file", unique=True
+        max_length=100,
+        help_text="Location of static catalog data file",
+        validators=[validate_catalog],
     )
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     updated = models.DateTimeField(auto_now=True, db_index=True, null=True)
