2a1c55298a20e9a28bd15624d9da43bf921da40c
rapidato-github-svc <93146671+rapidato-github-svc@users.noreply.github.com>
2022-07-14 11:29:54 -0400

    Bug/migrations am (#53)
    * ISPGBSS-1150 Added debug statement for db host and name
    
    * ISPGBSS-1150 Added debug statement for db host and name
    
    * ISPGBSS-1150 Fixed boolean issue
    
    * ISPGBSS-1150 Fixed boolean issue
    
    Co-authored-by: Azhar Mian <azharem@gmail.com>

--- blueprintapi/Environment.py
+++ blueprintapi/Environment.py
@@ -19,6 +19,7 @@ class Environment:
             if metadata_uri:
                 container_metadata = requests.get(metadata_uri).json()
                 hosts.append(container_metadata['Networks'][0]['IPv4Addresses'][0])
+        cors_origin = (os.environ.get("CORS_ALLOW_ALL_ORIGINS", "True").capitalize() == 'True')
         self.allowed_hosts      = hosts
         self.log_level          = os.environ.get("LOG_LEVEL", logging.INFO)
         self.oidc_config        = os.environ.get("OIDC_CONFIG")
@@ -28,13 +29,15 @@ class Environment:
         self.db_name            = os.environ.get("POSTGRES_DB_NAME")
         self.db_host            = os.environ.get("POSTGRES_DB_HOST")
         self.db_port            = os.environ.get("POSTGRES_DB_PORT", "5432")
-        self.cors_allow_origins = os.environ.get("CORS_ALLOW_ALL_ORIGINS", True)
+        self.cors_allow_origins = cors_origin
         self.debug              = os.environ.get("API_DEBUG", True)
         self.secret_key         = os.environ.get("SECRET_KEY", defaul_secret_key)
 
         # Log basic environment variables except db related for security reasons.
         logger.info("============== Environment Variables ================")
         logger.info(f"Allowed Hosts         : {self.allowed_hosts}")
+        logger.info(f"db host         :       {self.db_host}")
+        logger.info(f"db name         :       {self.db_name}")
         logger.info(f"Log Level             : {self.log_level}")
         logger.info(f"OIDC Config           : {self.oidc_config}")
         logger.info(f"Metrics Env           : {self.metrics_env}")
