905f63a1e26fa3a2adc44229922645489e8ac63f
Azhar Mian <azharem@gmail.com>
2022-07-13 22:08:18 -0400

    ISPGBSS-1150 Added debug statement for db host and name

--- blueprintapi/settings.py
+++ blueprintapi/settings.py
@@ -154,7 +154,7 @@ MEDIA_URL = "media/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CORS_ALLOW_ALL_ORIGINS = environment.get_cors_allow_origins()
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_HEADERS = [
     "accept",
     "accept-encoding",
