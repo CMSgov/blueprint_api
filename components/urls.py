381280496871e5fd2d99ad129ddb653274c091ee
Richard 'Dick' Moore <104368921+rmoore-theta@users.noreply.github.com>
2022-07-21 11:25:36 -0400

    new endpoint for returning list of unique component types in the db (#56)

--- components/urls.py
+++ components/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import ComponentDetailView, ComponentListSearchView, ComponentListView
+from .views import (
+    ComponentDetailView,
+    ComponentListSearchView,
+    ComponentListView,
+    ComponentTypeListView,
+)
 
 urlpatterns = [
     path("", ComponentListView.as_view(), name="component-list"),
     path("<int:pk>/", ComponentDetailView.as_view(), name="component-detail"),
     path("search/", ComponentListSearchView.as_view(), name="component-search"),
+    path("types/", ComponentTypeListView.as_view(), name="component-search"),
 ]
