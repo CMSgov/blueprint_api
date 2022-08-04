13b2924659537d36d2807ba2afb84a8c4a0cb13e
James White <jwhite@fearless.tech>
2022-08-04 11:42:00 -0400

    Add unauthenticated tests for a few endpoints

---
+++ blueprintapi/tests.py
@@ -0,0 +1,12 @@
+from rest_framework.test import APITestCase
+
+
+class UnauthenticatedAPITestCase(APITestCase):
+    def test_unauthenticated_request_returns_403(self):
+        test_cases = ('/api/projects/', '/api/components/', '/api/catalogs/', '/api/users/')
+
+        for test_case in test_cases:
+            with self.subTest(url=test_case):
+                response = self.client.get(test_case)
+
+                self.assertEqual(response.status_code, 401)
