df790be081882b7be012b8dff8443752a2641ca1
Joshua Alley <jauschalley@gmail.com>
2022-08-04 14:56:29 -0400

    add blank line

--- blueprintapi/views.py
+++ blueprintapi/views.py
@@ -2,6 +2,7 @@ from django.db import OperationalError, connection
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view(["GET"])
 def index(request):
     response = {"content": "Home"}
