57f9290805e8757f9b8eed89248176ac3bd958a3
James White <jwhite@fearless.tech>
2022-08-08 19:59:15 -0400

    Use custom object permissions

--- blueprintapi/settings.py
+++ blueprintapi/settings.py
@@ -190,7 +190,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'blueprintapi.permissions.StrictDjangoObjectPermissions',
     ],
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
