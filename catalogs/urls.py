eab43469b5151c2e3e967eb419f420d646d69a5e
James White <jwhite@fearless.tech>
2022-08-08 20:02:47 -0400

    Refactor catalog views with generics

--- catalogs/urls.py
+++ catalogs/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
-from .views import CatalogListView, get_all_controls, get_control_by_id
+from .views import CatalogListView, CatalogControlsView, CatalogControlDescriptionView
 
 urlpatterns = [
     path("", CatalogListView.as_view(), name="catalog-list"),
-    path("<int:catalog>/controls/all/", get_all_controls, name="get_all_controls"),
+    path("<int:catalog>/controls/all/", CatalogControlsView.as_view(), name="get_all_controls"),
     path(
-        "<int:catalog>/control/<str:control_id>/",
-        get_control_by_id,
-        name="get_control_by_id",
+        "<int:catalog>/control/<str:control_id>/", CatalogControlDescriptionView.as_view(), name="get_control_by_id",
     ),
 ]
