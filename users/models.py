56c8d8fed8dbc229c769d96e8b25be7524e45289
James White <jwhite@fearless.tech>
2022-08-10 17:32:47 -0400

    Add basic project permissions to user

--- users/models.py
+++ users/models.py
@@ -12,11 +12,16 @@ class User(AbstractUser):
 def add_default_user_perms(sender, instance, created, **kwargs):
     """After a User is created, assign object permissions to view/edit/delete to that user."""
     if created:
-        for code in ('change_user', 'delete_user', 'view_user', ):
+        user_perms = ('change_user', 'delete_user', 'view_user', )
+        project_perms = ('add_project', 'change_project', 'view_project', )
+
+        for code in user_perms + project_perms:
             try:
                 permission = Permission.objects.get(codename=code)
             except Permission.DoesNotExist:
                 break  # If the permissions don't exist, likely a setup step, so don't try and add perms
 
-            instance.user_permissions.add(permission)
-            assign_perm(code, instance, instance)
+            instance.user_permissions.add(permission)  # Model permissions
+
+            if code in user_perms:
+                assign_perm(code, instance, instance)  # Object permissions for user
