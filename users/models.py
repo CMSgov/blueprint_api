1777da91d34738ad5d064ae0e1ba30e4b720e679
James White <jwhite@fearless.tech>
2022-08-10 12:55:25 -0400

    Add default user permissions to itself

--- users/models.py
+++ users/models.py
@@ -1,6 +1,17 @@
-# from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, Permission
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from guardian.shortcuts import assign_perm
 
 
 class User(AbstractUser, PermissionsMixin):
     pass
+
+
+@receiver(post_save, sender=User)
+def add_default_user_perms(sender, instance, **kwargs):
+    """After a User is created, assign object permissions to view/edit/delete to that user."""
+    for code in ('change_user', 'delete_user', 'view_user', ):
+        permission = Permission.objects.get(codename=code)
+        instance.user_permissions.add(permission)
+        assign_perm(code, instance, instance)
