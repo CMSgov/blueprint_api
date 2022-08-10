63e71a22e829de0aee8091ad483f6d68e7a17550
James White <jwhite@fearless.tech>
2022-08-10 12:55:04 -0400

    Fix create user permissions

--- users/serializers.py
+++ users/serializers.py
@@ -6,4 +6,13 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "first_name", "last_name", "email", "is_active")
+        fields = ("id", "username", "password", "first_name", "last_name", "email", "is_active")
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
