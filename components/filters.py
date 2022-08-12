e87eeff4e16caecb31d942ba8437c4c9b451bbf3
James White <jwhite@fearless.tech>
2022-08-12 09:46:39 -0400

    Add component permissions filter

--- components/filters.py
+++ components/filters.py
@@ -1,7 +1,9 @@
 from django.db.models import Q
 from django_filters import rest_framework as filters
+from guardian.shortcuts import get_objects_for_user
+from rest_framework.filters import BaseFilterBackend
 
-from .models import Component
+from components.models import Component
 
 
 class ComponentFilter(filters.FilterSet):
@@ -17,4 +19,23 @@ class ComponentFilter(filters.FilterSet):
     def keyword_search(self, queryset, name, value):
         return queryset.filter(
             Q(title__icontains=value) | Q(description__icontains=value)
+        )
+
+
+class ComponentPermissionsFilter(BaseFilterBackend):
+    """A filter backend that limits results to those where the requesting user
+    has read object level permissions.
+    """
+    perm_format = '%(app_label)s.view_%(model_name)s'
+
+    def filter_queryset(self, request, queryset, view):
+        user = request.user
+        permission = self.perm_format % {
+            'app_label': queryset.model._meta.app_label,
+            'model_name': queryset.model._meta.model_name,
+        }
+
+        return (
+                get_objects_for_user(user, permission, queryset, accept_global_perms=False)
+                | queryset.filter(status=Component.Status.PUBLIC)
         )
