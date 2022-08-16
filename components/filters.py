b3f6a81d3c11424c56208c51d39b9d6d27cbbd67
James White <jwhite@fearless.tech>
2022-08-16 15:06:44 -0400

    Remove order_by in ComonentPermissionsFilter

--- components/filters.py
+++ components/filters.py
@@ -38,5 +38,4 @@ class ComponentPermissionsFilter(BaseFilterBackend):
         return (
                 get_objects_for_user(user, permission, queryset, accept_global_perms=False)
                 .union(queryset.filter(status=Component.Status.PUBLIC))
-                .order_by("pk")
         )
