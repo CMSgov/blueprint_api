218f02be437ef53c0ceb5ffc4ef0bd0d14c5567e
James White <jwhite@fearless.tech>
2022-08-30 15:56:15 -0400

    Refactor load_catalog command to include option to load included catalogs

--- catalogs/management/commands/load_catalog.py
+++ catalogs/management/commands/load_catalog.py
@@ -1,4 +1,7 @@
+import re
+
 from pathlib import Path
+from typing import Tuple
 
 from django.core.files import File
 from django.core.management.base import BaseCommand, CommandError
@@ -16,25 +19,54 @@ class Command(BaseCommand):
         parser.add_argument("--name", type=str)
         parser.add_argument("--source", type=str, default=None)
         parser.add_argument("--catalog-version", type=str, default=None)
+        parser.add_argument("--load-standard-catalogs", action="store_true")
 
     def handle(self, *args, **options):
-        input_file = Path(options["catalog_file"])
+        if options["load_standard_catalogs"]:
+            self._load_standards()
+        else:
+            input_file = Path(options["catalog_file"])
+            name = input_name if (input_name := options["name"]) else input_file.name
+            create_kwargs = {}
+            for arg in ("source", "catalog_version", "impact_level"):
+                if value := options[arg]:
+                    create_kwargs[arg if arg != "catalog_version" else "version"] = value
+
+            self._load_catalog(input_file, name, **create_kwargs)
+
+    def _load_catalog(self, input_file: Path, name: str, **catalog_args):
+        if Catalog.objects.filter(name=name).exists():
+            self.stdout.write(self.style.WARNING("Catalog, %s has already been loaded. Skipping." % name))
+            return
 
         try:
             with open(input_file, mode="rb") as catalog:
                 file = File(catalog)
-                name = input_name if (input_name := options["name"]) else input_file.name
-                create_kwargs = {"file_name": file, "name": name}
-
-                for arg in ("source", "catalog_version", "impact_level"):
-                    if value := options[arg]:
-                        create_kwargs[arg if arg != "catalog_version" else "version"] = value
-
-                Catalog.objects.create(**create_kwargs)
-
+                Catalog.objects.create(file_name=file, name=name, **catalog_args)
         except IntegrityError as exc:
             raise CommandError(f"Error in creating new catalog: {exc}") from exc
         except (IOError, FileNotFoundError) as exc:
             raise CommandError(f"Error loading catalog file: {exc}") from exc
 
         self.stdout.write(self.style.SUCCESS('Successfully ingested catalog "%s"' % name))
+
+    @staticmethod
+    def _parse_standard_catalog_path(path: Path) -> Tuple[str, str, str]:
+        """Parse Catalog information from the standard catalog naming convention and path."""
+        version = f"CMS_ARS_{path.parent.name.replace('.', '_')}"
+        impact_level_match = re.search(r"(?P<impact_level>high|HIGH|moderate|MODERATE|low|LOW)", path.name)
+        impact_level = impact_level_match.groupdict()["impact_level"].lower()
+        name = f"{version}_{impact_level.upper()}"
+
+        return version, impact_level, name
+
+    def _load_standards(self):
+        """Load standard catalogs from catalogs/data"""
+        source = "https://github.com/CMSgov/ars-machine-readable"  # See catalogs/data/README.md
+
+        catalogs_path = Path(__file__).parents[2] / "data"
+        catalog_files = list(catalogs_path.rglob("*json"))
+        catalog_defs = [self._parse_standard_catalog_path(path) for path in catalog_files]
+
+        for (version, impact_level, name), file in zip(catalog_defs, catalog_files):
+            self._load_catalog(input_file=file.relative_to(file.parents[3]), name=name, version=version, impact_level=impact_level, source=source)
